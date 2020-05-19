9f5936d7bf154228482dad283ce4e71181604516
Ryan Quigley <ryan.quigley32@outlook.com>
2020-05-19 12:34:29 -0400

    update

--- frontend/src/components/EmployeeRoles.vue
+++ frontend/src/components/EmployeeRoles.vue
@@ -1,20 +1,36 @@
 <template>
   <div>
     <h1>Employee Roles</h1>
+    <div class="form-group" v-for="(input,k) in roles" :key="k">
+    <input type="text" class="form-control" v-model="input.name">
+    <span>
+        <i class="fas fa-minus-circle" @click="remove(k)" v-show="k || ( !k && roles.length > 1)"></i>
+        <i class="fas fa-plus-circle" @click="add(k)" v-show="k == roles.length-1"></i>
+    </span>
+</div>
     
   </div>
 </template>
 
 <script>
-import axios from "axios";
-
 export default {
   components: {},
   data() {
     return {
+      roles: [
+        {
+        name: ''
+        }
+      ]
     };
   },
   methods: {
+    add(index) {
+        this.inputs.push({ name: '' });
+        },
+    remove(index) {
+      this.inputs.splice(index, 1);
+        }
     
   }
 };
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <h1>Landing Page</h1>
     <employee-invite></employee-invite>
+    <employee-roles></employee-roles>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import EmployeeInvite from "../components/EmployeeInvite";
+import EmployeeRoles from "../components/EmployeeRoles";
 export default {
   name: 'home',
   components: {
-    EmployeeInvite
+    EmployeeInvite,
+    EmployeeRoles
   }
 }
 
