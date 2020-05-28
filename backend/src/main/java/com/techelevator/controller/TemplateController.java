4e424398e45b7d13a07d13ec714f910d1641d718
Trevor Southworth <tsouthworth8@gmail.com>
2020-05-28 16:20:22 -0400

    New ShiftTemplate functionality working.

--- backend/src/main/java/com/techelevator/controller/TemplateController.java
+++ backend/src/main/java/com/techelevator/controller/TemplateController.java
@@ -1,5 +1,6 @@
 package com.techelevator.controller;
 
+import java.time.LocalTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.techelevator.authentication.AuthProvider;
+import com.techelevator.model.UserDao;
 import com.techelevator.model.templates.ShiftTemplate;
 import com.techelevator.model.templates.ShiftTemplateDAO;
 import com.techelevator.model.templates.StringShiftTemplate;
@@ -23,6 +25,8 @@ public class TemplateController {
 	private AuthProvider auth;
 	@Autowired
 	private ShiftTemplateDAO tempDAO;
+	@Autowired
+	private UserDao userDAO;
 	
 	@RequestMapping(path = "/getAll", method = RequestMethod.GET)
 	public List<ShiftTemplate> getAllShiftTemplates() {
@@ -32,6 +36,19 @@ public class TemplateController {
 	
 	@RequestMapping(path = "/new", method = RequestMethod.POST)
 	public String newShiftTemplate(@RequestBody StringShiftTemplate template) {
+		System.out.println(userDAO.getCompanyIdByUserId(auth.getCurrentUser().getId()));
+		
+		int startHour = Integer.parseInt(template.getStartTime().split(":")[0]);
+		int startMinute = Integer.parseInt(template.getStartTime().split(":")[1]);
+		LocalTime startTime = LocalTime.of(startHour, startMinute, 0, 0);
+		
+		int endHour = Integer.parseInt(template.getEndTime().split(":")[0]);
+		int endMinute = Integer.parseInt(template.getEndTime().split(":")[1]);
+		LocalTime endTime = LocalTime.of(endHour, endMinute, 0, 0);
+		
+		ShiftTemplate finalTemplate = new ShiftTemplate(startTime, endTime, template.getAllowedShiftRoles());
+		tempDAO.saveCompanyShiftTemplate(userDAO.getCompanyIdByUserId(auth.getCurrentUser().getId()), finalTemplate);
+		
 		System.out.println("Controller reached");
 		return "Haaayyy";
 	}
